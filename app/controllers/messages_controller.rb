c1bff60374e3cc7de1153edf3bd5b62186ea2dd4
Yukie-Sako <vetkas4astia@gmail.com>
2018-11-08 18:55:14 +0900

    update: controllers
    *コントローラ関連のupdate

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,9 @@
 class MessagesController < ApplicationController
+  def index
+  end
 
+  def create
+    Message.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+
+  end
 end
