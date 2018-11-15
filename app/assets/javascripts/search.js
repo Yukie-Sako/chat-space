5a1d24b3387fa00c73f26856a9ad2afb99771223
Yukie-Sako <vetkas4astia@gmail.com>
2018-11-15 13:42:18 +0900

    update:
    * メンバーの追加・削除機能の追加

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,64 +1,62 @@
 $(function() {
-  function buildHTML(user) {
+
+  var user_search_result = $('#user-search-result');
+  var chat_member_list = $('#chat-group-users');
+
+  function appendUser(user) {
     var html =
     `<div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${ user.name }</p>
       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=" ${ user.id }" data-user-name="${ user.name }">追加</a>
-</div>`
+    </div>`
+    user_search_result.append(html);
+  }
 
+  function addUser(id, name) {
+    var html =
+    `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${ name }</p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--remove" data-user-id="${ id }" data-user-name="${ name }">削除</a>
+     </div>`
+   return html;
+  }
 
-return html;
-}
 
   $('#user-search-field').on('keyup', function(){
     var input = $(this).val();
+
     $.ajax({
       url: '/users',
       type: 'GET',
-      data: {keyword: input},
+      data: { keyword: input },
       dataType: 'json'
     })
+
     .done(function(users){
       $('#user-search-result').empty();
-      users.forEach(function(user){
-        var insertHTML = buildHTML(user);
-        $('#user-search-result').append(insertHTML);
-      });
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("該当するユーザーはいません");
+      }
     })
     .fail(function(){
       alert('error');
-    });
+    })
   });
+
+  $(document).on('click', ".chat-group-user__btn--add", function (){
+    var id = $(this).data('user-id')
+    var name = $(this).data('user-name')
+    var html = addUser(id, name)
+    $('#chat-group-users').append(html)
+    $(this).parent().remove();
+    })
+
+  $(document).on('click', "chat-group-user__btn--remove", function(){
+    $(this).parent().remove();
+  })
 });
-
-// $(function() {
-//   function buildHTML(users) {
-//     var html =` <p class="chat-group-user__name">${ user.name }</p>
-//   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">削除</a>
-// </div>`
-// };
-// return html;
-
-// });
-
-//   $('user-search-add').on('submit', function(e){
-//     e.preventDefault();
-//     var $form = $(this);
-//     formData = new FormData(this);
-//     $.ajax({
-//       url: '/users',
-//       type: 'GET',
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       var insertHTML = buildHTML(data);
-//       $('#chat-group-users').append(insertHTML)
-//       $('#user-search-result')[0].reset();
-//     })
-//     .fail(function(){
-//       alert('error');
-//     });
-//   });
